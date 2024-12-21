9a06a875e6681d10f5725a7aef280ccb6d3f10fd
Jared A Sutton <jaredasutton@gmail.com>
2024-12-21 04:10:36 +0000

    feat(go-wasm): Add console.log.

--- apps/go-wasm/src/main.go
+++ apps/go-wasm/src/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"syscall/js"
+	"fmt"
 )
 
 func main() {
 	alert := js.Global().Get("alert")
 	alert.Invoke("Hello, WebAssembly!")
+
+	fmt.Println("Hello, WebAssembly!")
 }
